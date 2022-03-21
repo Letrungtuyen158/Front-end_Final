29112c35afd13acbf0ad25eedd93149cc8388460
Your Name <you@example.com>
2022-03-21 22:50:44 +0700

    fix: changge Localsto , redux

--- src/store/reducers/rootReducer.js
+++ src/store/reducers/rootReducer.js
@@ -19,9 +19,15 @@ const userPersistConfig = {
   whitelist: ["isLoggedIn", "userInfo", "language"],
 };
 
+const appPersisConfig = {
+  ...persistCommonConfig,
+  key: "app",
+  whitelist: ["language"],
+};
+
 export default (history) =>
   combineReducers({
     router: connectRouter(history),
     user: persistReducer(userPersistConfig, userReducer),
-    app: appReducer,
+    app: persistReducer(appPersisConfig, appReducer),
   });
